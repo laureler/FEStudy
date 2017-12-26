0198d51e180f824a86bafa9bb6a92639579f2165
lucas <laureler@163.com>
2017-12-26 12:00:49 +0800

    添加 JavaScript 武器

---
+++ ECMAScript/utils/isNull.js
@@ -0,0 +1,9 @@
+/**
+ *  判断一个JS对象是否是空
+ * @param obj           要判断的对象
+ * @param config        要判断的规则
+ * @return {boolean}    返回是否为空，若空，返回true. 否则返回false
+ */
+function isNullObject (obj,config) {
+  return   true
+}
