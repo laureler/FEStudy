9965dad565cbf6997cf430a57769421a3aa12fef
lucas <laureler@163.com>
2017-12-29 09:58:19 +0800

    处理图片

--- webpack/webpackAll/src/index.js
+++ webpack/webpackAll/src/index.js
@@ -1,12 +1,16 @@
 // index.js中使用es6语法
 import _ from 'lodash'
 import  './style.css'
+import  myJpg  from './test.jpg'
 
 function component () {
   let elementById = document.createElement('span')
   // lodash import by this script
   elementById.innerHTML = _.join(['hello', 'webpack', '   '])
   elementById.classList.add('hello')
+  let image = new Image()
+  image.src = myJpg
+  elementById.appendChild(image)
   return elementById
 }
 // 执行 npx webpack src/index.js dist/bundle.js 编译脚本
