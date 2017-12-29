0ad2417cc6a35bd51d50da9c758a410fd4fa6486
lucas <laureler@163.com>
2017-12-29 14:01:17 +0800

    webpack html plugin 插件错误

--- webpack/webpackAll/src/index.js
+++ webpack/webpackAll/src/index.js
@@ -1,16 +1,18 @@
 // index.js中使用es6语法
 import _ from 'lodash'
-import  './style.css'
-import  myJpg  from './test.jpg'
+import printMe from './print.js'
+
 
 function component () {
   let elementById = document.createElement('span')
   // lodash import by this script
   elementById.innerHTML = _.join(['hello', 'webpack', '   '])
   elementById.classList.add('hello')
-  let image = new Image()
-  image.src = myJpg
-  elementById.appendChild(image)
+  var btn = document.createElement('button');
+  btn.innerHTML = '点我'
+  btn.onclick = printMe
+  elementById.appendChild(btn)
+
   return elementById
 }
 // 执行 npx webpack src/index.js dist/bundle.js 编译脚本
--- webpack/webpackAll/webpack.config.js
+++ webpack/webpackAll/webpack.config.js
@@ -2,39 +2,16 @@
 // 其运行环境应该是在 node.js服务器上来执行的逻辑
 
 const path = require('path')
+const  HtmlWebpackPlugin = require('html-webpack-plugin')
 // node.js 导出文件 导出一个对象实例
 module.exports = {
-  entry: './src/index.js',
-
+  entry: {
+    app:'./src/index.js',
+    print:'./src/print.js'
+  },
+  plugins:[new HtmlWebpackPlugin()],
   output: {
-    filename: 'bundle.js',
-    // path 是node.js中的方法 dirname: C:\dev\projects\JavaScriptStudy\webpack\03使用配置文件 当前文件所在文件夹的绝对路径
-    // resolve 是指 吧__dirname dist拼接为一个绝对路径，若不能拼接，则返回当前目录 也就是说 应该返回了 __dirname/dist文件
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        //webpack 根据正则表达式，来确定应该查找哪些文件，并将其提供给指定的 loader。在这种情况下，以 .css 结尾的全部文件，都将被提供给 style-loader 和 css-loader。
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use:[
-          'file-loader'
-        ]
-      },
-      {
-        test:/\.(woff|woff2|eot|ttf|otf)$/,
-        use:[
-          'file-loader'
-        ]
-      },
-    ]
-  }
 }
-console.log(__dirname)
