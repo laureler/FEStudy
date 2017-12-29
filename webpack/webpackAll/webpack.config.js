1fe96bdaf0a6b4939fdcf8583f7cedde119dec59
lucas <laureler@163.com>
2017-12-29 10:11:16 +0800

    加载字体

--- webpack/webpackAll/webpack.config.js
+++ webpack/webpackAll/webpack.config.js
@@ -28,6 +28,12 @@ module.exports = {
           'file-loader'
         ]
       },
+      {
+        test:/\.(woff|woff2|eot|ttf|otf)$/,
+        use:[
+          'file-loader'
+        ]
+      },
     ]
   }
 }
