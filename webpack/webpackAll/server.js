06f3a41eb8deb69f0fa9f2a5daa407199d2c3a2e
lucas <laureler@163.com>
2018-01-02 14:43:51 +0800

    服务器部署 自动打包配置

---
+++ webpack/webpackAll/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+
+const app = express()
+const config = require('./webpack.config')
+const compiler = webpack(config)
+
+//tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as  a base
+
+app.use(webpackDevMiddleware(compiler,{
+  publicPath:config.output.publicPath
+}))
+
+// server the files on port 3000
+app.listen(3000,function () {
+  console.log('example app listening on port 3000!\n')
+})
