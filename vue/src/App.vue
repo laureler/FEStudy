60bbed0ad691e830d3ba974d42aadc7dacc962bc
lucas <laureler@163.com>
2018-04-12 18:11:48 +0800

    完善事件冒泡机制

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <el-tabs v-model="editableTabsValue2" type="tabCard" closable @tab-remove="removeTab">
-      <el-tab-pane
-        v-for="(item, index) in editableTabs2"
-        :key="item.name"
-        :label="item.title"
-        :name="item.name"
-      >
-        <div v-html="item.content"></div>
-      </el-tab-pane>
-
-    </el-tabs>
+    <div id="section">
+      <div class="layui-table-cell laytable-cell-11-sex"> <!-- 这里的 checked 的状态只是演示 -->
+        <input type="checkbox" name="sex"
+               lay-skin="switch"
+               lay-text="女|男"
+               lay-filter="test-table-sexDemo"
+               value="10003"
+               data-json="%7B%22LAY_INDEX%22%3A4%2C%22id%22%3A10003%2C%22username%22%3A%22user-3%22%2C%22sex%22%3A%22%E5%A5%B3%22%2C%22city%22%3A%22%E5%9F%8E%E5%B8%82-3%22%2C%22sign%22%3A%22%E7%AD%BE%E5%90%8D-3%22%2C%22experience%22%3A362%2C%22logins%22%3A157%2C%22wealth%22%3A37117017%2C%22classify%22%3A%22%E8%AF%97%E4%BA%BA%22%2C%22score%22%3A68%2C%22LAY_TABLE_INDEX%22%3A3%7D"
+               checked="">
+        <div class="layui-unselect layui-form-switch layui-form-onswitch" lay-skin="_switch"><em>女</em><i></i></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,7 +21,7 @@
 
   export default {
     name: 'App',
-    data() {
+    data () {
       return {
         editableTabsValue2: '2',
         editableTabs2: [{
@@ -36,72 +37,71 @@
       }
     },
     methods: {
-      handleTabsEdit(targetName, action) {
+      handleTabsEdit (targetName, action) {
 
         if (action === 'add') {
-          let newTabName = ++this.tabIndex + '';
+          let newTabName = ++this.tabIndex + ''
           this.editableTabs.push({
             title: 'New Tab',
             name: newTabName,
             content: 'New Tab content'
-          });
-          this.editableTabsValue = newTabName;
+          })
+          this.editableTabsValue = newTabName
         }
         if (action === 'remove') {
           debugger
-          let tabs = this.editableTabs;
-          let activeName = this.editableTabsValue;
+          let tabs = this.editableTabs
+          let activeName = this.editableTabsValue
           if (activeName === targetName) {
             tabs.forEach((tab, index) => {
               if (tab.name === targetName) {
-                let nextTab = tabs[index + 1] || tabs[index - 1];
+                let nextTab = tabs[index + 1] || tabs[index - 1]
                 if (nextTab) {
-                  activeName = nextTab.name;
+                  activeName = nextTab.name
                 }
               }
-            });
+            })
           }
 
-          this.editableTabsValue = activeName;
-          this.editableTabs = tabs.filter(tab =>
-            {
+          this.editableTabsValue = activeName
+          this.editableTabs = tabs.filter(tab => {
               tab.name !== targetName
             }
-          );
+          )
         }
       },
-      addTab(targetName) {
-        let newTabName = ++this.tabIndex + '';
+      addTab (targetName) {
+        let newTabName = ++this.tabIndex + ''
         this.editableTabs2.push({
           title: 'New Tab',
           name: newTabName,
           content: 'New Tab content'
-        });
-        this.editableTabsValue2 = newTabName;
+        })
+        this.editableTabsValue2 = newTabName
       },
-      removeTab(targetName) {
+      removeTab (targetName) {
         //当前所有逇tabs选项数据
-        let tabs = this.editableTabs2;
+        let tabs = this.editableTabs2
         //当前获得焦点的tab
-        let activeName = this.editableTabsValue2;
+        let activeName = this.editableTabsValue2
         //若是当前焦点
         if (activeName === targetName) {
           tabs.forEach((tab, index) => {
             if (tab.name === targetName) {
-              let nextTab = tabs[index + 1] || tabs[index - 1];
+              let nextTab = tabs[index + 1] || tabs[index - 1]
               if (nextTab) {
-                activeName = nextTab.name;
+                activeName = nextTab.name
               }
             }
-          });
+          })
         }
         //若不是当前脚垫 则设置为当前焦点
-        this.editableTabsValue2 = activeName;
+        this.editableTabsValue2 = activeName
         //并且过滤 targetName与tab的name属性不一致
         this.editableTabs2 = tabs.filter(tab => {
-            debugger
-            tab.name !== targetName
-        });
+          debugger
+          tab.name !== targetName
+        })
       }
     },
     components: {
@@ -111,6 +111,30 @@
 </script>
 
 <style>
+  .layui-form-onswitch{
+    border-color: #5FB878;
+    background-color: #5FB878;
+  }
+  .layui-table-cell {
+    height: 28px;
+    line-height: 28px;
+    padding: 0 15px;
+    position: relative;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    box-sizing: border-box;
+  }
+  .laytable-cell-11-sex {
+    width: 85px;
+  }
+  #section {
+    position: relative;
+    margin: 100px auto;
+    width: 500px;
+    height: 300px;
+  }
+
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
