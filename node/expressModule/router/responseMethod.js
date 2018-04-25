bf7b70b914eb629de4176d0b849b89d6eecd66f7
lucas <laureler@163.com>
2018-04-25 18:40:03 +0800

    添加 路由响应方法

--- node/expressModule/router/responseMethod.js
+++ node/expressModule/router/responseMethod.js
@@ -2,5 +2,16 @@
 var express = require('express')
 var app = express()
 
+app.get('/index',(req,resp)=>{
+  res.download()//提示下载文件
+  res.end() //中介响应处理流程
+  res.json()  //发送一个json格式的响应
+  res.jsonp() //发送一个支持jsonp的json格式的响应
+  res.redirect() // todo 重定向请求
+  res.render()  // todo 渲染视图模板 ？
+  res.send()
+  res.sendfile()
+  res.sendStatus()
+})
 app.listen(3000)
 console.log('listening http://127.0.0.1:3000')
